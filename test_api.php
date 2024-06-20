c89ee35a0a88a5e07ff20c3b84f2d6f891ccd3c7
Sergey-Smolkin-Leasoft <143398413+Sergey-Smolkin-Leasoft@users.noreply.github.com>
2024-06-20 20:21:57 +0300

    trading bot

---
+++ test_api.php
@@ -0,0 +1,34 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use Binance\Spot;
+use GuzzleHttp\Client;
+
+$config = require 'config/config.php';
+
+$apiKey = trim($config['binance']['apiKey']);
+$secretKey = trim($config['binance']['secretKey']);
+
+$guzzleClient = new Client([
+    'verify' => false, // Отключить проверку SSL для тестирования
+    'debug' => true,   // Включить отладочную информацию
+]);
+
+$client = new Spot([
+    'apiKey' => $apiKey,
+    'secretKey' => $secretKey,
+    'http_client_handler' => $guzzleClient,
+    'recvWindow' => 60000,
+]);
+
+try {
+    $response = $client->account();
+    print_r($response);
+} catch (Exception $e) {
+    echo 'Ошибка: ',  $e->getMessage(), "\n";
+    if ($e instanceof \GuzzleHttp\Exception\ClientException) {
+        $response = $e->getResponse();
+        echo 'Ответ: ', $response->getBody()->getContents(), "\n";
+    }
+}
