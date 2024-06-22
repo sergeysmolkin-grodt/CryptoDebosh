01839ad309277ba4a46b6422e3d75ff2ec378324
Sergey-Smolkin-Leasoft <143398413+Sergey-Smolkin-Leasoft@users.noreply.github.com>
2024-06-22 16:35:07 +0300

    Implement HttpClientAdapter class

--- src/Shared/Adapters/HttpClientAdapter.php
+++ src/Shared/Adapters/HttpClientAdapter.php
@@ -4,7 +4,88 @@ declare(strict_types=1);
 
 namespace App\Shared\Adapters;
 
-class HttpClientAdapter
-{
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\GuzzleException;
+use Psr\Http\Message\ResponseInterface;
 
+final class HttpClientAdapter
+{
+    /**
+     * @var Client
+     */
+    private $client;
+
+    /**
+     * HttpClientAdapter constructor.
+     *
+     * @param Client $client
+     */
+    public function __construct(Client $client)
+    {
+        $this->client = $client;
+    }
+
+    /**
+     * Perform a GET request.
+     *
+     * @param string $url
+     * @param array $headers
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public function get(string $url, array $headers = []): ResponseInterface
+    {
+        return $this->client->request('GET', $url, [
+            'headers' => $headers,
+        ]);
+    }
+
+    /**
+     * Perform a POST request.
+     *
+     * @param string $url
+     * @param array $data
+     * @param array $headers
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public function post(string $url, array $data = [], array $headers = []): ResponseInterface
+    {
+        return $this->client->request('POST', $url, [
+            'headers' => $headers,
+            'json' => $data,
+        ]);
+    }
+
+    /**
+     * Perform a PUT request.
+     *
+     * @param string $url
+     * @param array $data
+     * @param array $headers
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public function put(string $url, array $data = [], array $headers = []): ResponseInterface
+    {
+        return $this->client->request('PUT', $url, [
+            'headers' => $headers,
+            'json' => $data,
+        ]);
+    }
+
+    /**
+     * Perform a DELETE request.
+     *
+     * @param string $url
+     * @param array $headers
+     * @return ResponseInterface
+     * @throws GuzzleException
+     */
+    public function delete(string $url, array $headers = []): ResponseInterface
+    {
+        return $this->client->request('DELETE', $url, [
+            'headers' => $headers,
+        ]);
+    }
 }
